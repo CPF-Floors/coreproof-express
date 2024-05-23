810bf7be18a59997dae0dea00424581bf0417464
Sebas <sebas@cpffloors.com>
2024-05-23 11:25:27 -0400

    cors commit

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors"
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import corsOptions from "./libs/corsOptions.js";
 
 const server = express();
 
+server.use(cors(corsOptions))
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cookieParser());
